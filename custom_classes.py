663a36aefa32065732850fa748e628a49ecfff48
eraycan.uyanik <eraycan.uyanik@rapsodo.com>
2026-03-12 21:46:58 +0300

    v1.0: calculator base

---
+++ custom_classes.py
@@ -0,0 +1,4 @@
+class Calculator:
+    def __init__(self):
+        # Initialize a private attribute to 0
+        self._current_val = 0
---
+++ main.py
@@ -0,0 +1,7 @@
+from custom_classes import Calculator
+
+# Create an instance of the Calculator class
+my_calc = Calculator()
+
+# Print the private _current_val attribute
+print(my_calc._current_val)
