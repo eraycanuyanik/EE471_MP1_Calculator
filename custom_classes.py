e7579109a4d9f77271491b2665949566eeb21da0
eraycan.uyanik <eraycan.uyanik@rapsodo.com>
2026-03-12 21:59:36 +0300

    feat: multiply method created

--- custom_classes.py
+++ custom_classes.py
@@ -2,3 +2,7 @@ class Calculator:
     def __init__(self):
         # Initialize a private attribute to 0
         self._current_val = 0
+
+    def multiply(self, x, y):
+        self._current_val = x * y
+        return self._current_val
