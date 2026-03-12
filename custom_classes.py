918f867e5019c535447877a2dce5b9fc6b7851b8
eraycan.uyanik <eraycan.uyanik@rapsodo.com>
2026-03-12 22:00:07 +0300

    feat: divide method created

--- custom_classes.py
+++ custom_classes.py
@@ -2,3 +2,9 @@ class Calculator:
     def __init__(self):
         # Initialize a private attribute to 0
         self._current_val = 0
+
+    def divide(self, x, y):
+        if y == 0:
+            raise ValueError("Cannot divide by zero")
+        self._current_val = x / y
+        return self._current_val
